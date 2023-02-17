34f8363ad0b474fd4397e6ee97656918327f5ff8
airnam007 <enamek16@gmail.com>
2023-02-16 16:12:09 -0800

    hello puts commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line with the puts function
- * Return : 0 (Success)
+ * main - Entry point with puts functions
+ * Return: (0); (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like buiding a multilingual puzzle");
 return (0);
 }
-
