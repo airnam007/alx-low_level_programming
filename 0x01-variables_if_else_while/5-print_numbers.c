737fe1673cdbb30179ed6b602097c669a46f6041
airnam007 <enamek16@gmail.com>
2023-02-17 18:48:55 -0800

    numbers base 10 commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Displays all values of base 10 with for
+ *
+ * Return: 0(Success)
+ */
+
+int main(void)
+{
+
+	for (int i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+
+	return (0);
+}
