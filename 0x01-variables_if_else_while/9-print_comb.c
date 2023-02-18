b848c35c1b8d1bf1de5391d94298e538d26db7f4
airnam007 <enamek16@gmail.com>
2023-02-17 19:58:26 -0800

    patience, persistence, perspiration

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Display Single digits clearly
+ * Return: 0(Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		puchar(i);
+		if (i != '9')
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	}
+
+	return (0);
+}
