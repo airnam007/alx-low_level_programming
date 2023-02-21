233a3043a339a49278496f22908905ad0451e454
airnam007 <enamek16@gmail.com>
2023-02-21 11:22:00 -0800

    check for sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - checks the sign of a number
+ * @n: number to be checked
+ * Return: 0(Success)
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+	_putchar('\n');
+}
