16b7c4fb4b1041f29879dd6dacef26e30849bdf6
airnam007 <enamek16@gmail.com>
2023-02-23 12:38:47 -0800

    Numbers speak for themselves

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,3 +5,17 @@ int _isdigit(int c);
 int mul(int a, int b);
 
 void print_numbers(void);
+
+#include <unistd.h>
+
+/**
+ * _putchar - writes the chracter c to stdout
+ * Return: 1
+ */
+
+int _putchar(char c)
+{
+
+        return (write(1, &c, 1));
+}
+~
