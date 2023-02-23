5d8fe67ab99634e137a5e716383f34d8e4fe69fb
airnam007 <enamek16@gmail.com>
2023-02-23 09:59:39 -0800

    Numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - check the code for print_numbers functions
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_numbers();
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Displays numbers
+ * Return: 0(Success)
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	if (a < 10)
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+}
