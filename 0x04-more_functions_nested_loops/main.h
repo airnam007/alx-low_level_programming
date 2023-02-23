b28406b4607bd560ec63d1a8eb8e9d13ca0253dc
airnam007 <enamek16@gmail.com>
2023-02-23 14:06:43 -0800

    Numbers speak for themselves

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _isupper(int c);
 
 int _isdigit(int c);
@@ -8,15 +11,4 @@ void print_numbers(void);
 
 void print_most_numbers(void);
 
-#include <unistd.h>
-
-/**
- * _putchar - writes the chracter c to stdout
- * Return: 1
- */
-
-int _putchar(char c)
-{
-
-        return (write(1, &c, 1));
-}
+#endif
