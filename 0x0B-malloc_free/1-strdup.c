600d9c163034c4a5c32f34ef80901b61ec21bbd4
airnam007 <enamek16@gmail.com>
2023-03-14 05:35:24 -0700

    no imigination no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - duplicate to new memory space location
+ * @str: char
+ * Return: 0
+ */
+
+char *_strdup(char *str)
+
+{
+	char *airnam;
+
+	int i, r = 0;
+
+	if (str == NULL)
+
+		return (NULL);
+
+	i = 0;
+
+	while (str[i] != '\0')
+
+		i++;
+
+	airnam = malloc(sizeof(char) * (i + 1));
+
+	if (airnam == NULL)
+
+		return (NULL);
+
+	for (r = 0; str[r]; r++)
+
+		airnam[r] = str[r];
+
+	return (airnam);
+}
