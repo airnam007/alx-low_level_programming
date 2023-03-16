6f9336f6d59e58e60dc3ec88088546cc00f30bcb
airnam007 <enamek16@gmail.com>
2023-03-16 10:40:07 -0700

    array range commit

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -2,32 +2,24 @@
 #include "main.h"
 
 /**
- * *array_range - creates an array of integers
- * @min: minimum range of values stored
- * @max: maximum range of values stored and number of element
- * Return: pointer to the new array
+ * array_range - creates an array of integers
+ * @min: minimum
+ * @max: maximum
+ * Return: array
  */
 
 int *array_range(int min, int max)
 {
-	int *ptr;
-
-	int i, size;
+	int *arr, i = 0, t = min;
 
 	if (min > max)
+		return (0);
+	arr = malloc((max - min + 1) * sizeof(int));
 
-		return (NULL);
+	if (!arr)
+		return (0);
+	while (i <= max - min)
+		ar[i++] = t++;
 
-	size = max - min + 1;
-
-	ptr = malloc(sizeof(int) * size)
-
-	if (ptr == NULL)
-		return (NULL);
-
-	for (i = 0; min <= max; i++)
-	{
-		ptr[i] = min++;
-	}
-	return (ptr);
+	return (arr);
 }
