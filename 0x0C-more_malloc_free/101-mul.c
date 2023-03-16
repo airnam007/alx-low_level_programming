3f83a9863c56a731d2bbf481faa9efe42060d6a1
airnam007 <enamek16@gmail.com>
2023-03-16 11:19:27 -0700

    array range commit

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -19,10 +19,13 @@ int main(int argc, char *argv[])
 	}
 	for (a = 1; a < argc; a++)
 	{
-		for (b = 0; argv[a][b] > 57 || argv[a][b] < 48)
+		for (b = 0; argv[a][b] != '\0'; b++)
 		{
+			if (argv[a][b] > 57 || argv[a][b] < 48)
+			{
 			printf("Error\n");
 			exit(98);
+			}
 		}
 	}
 	mul = atol(argv[1]) * atol(argv[2]);
