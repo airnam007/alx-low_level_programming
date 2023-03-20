6db8e7249304ff0a7dc4c5d981ad77f0947c1717
airnam007 <enamek16@gmail.com>
2023-03-20 11:18:58 -0700

    uncondtional love from dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,10 +17,10 @@ struct dog
 };
 
 /**
- * my_dog - typedef for struct dog
+ * dog_c - typedef for struct dog
  * Description: longer description for typedef
  */
-typedef struct dog my_dog;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
