062bc9f455c517d9981e586b7fc89cb5e7b8e94b
airnam007 <enamek16@gmail.com>
2023-03-20 09:28:47 -0700

    poppy commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a basic info on dog
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ * Description: longer description for struct dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
